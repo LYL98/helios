5dab966c9200283d70d06bd146fe89a37583156e
胖墩-XXXXXXXXL <wx_9bbb4f3b386f4e0e8434a20e5378cc45@git.code.tencent.com>
2019-07-17 11:06:55 +0800

    添加Sentry

--- src/main.js
+++ src/main.js
@@ -6,6 +6,24 @@ import store from './store';
 import router from './router';
 import { DataHandle } from '@/util';
 
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
+
+// 服务器地址
+let url = window.location.origin;
+let isPro = url.indexOf('apple.pgyscm.com') >= 0;
+if(isPro){
+  Sentry.init({
+    dsn: 'http://3f58bbfce036428b83b8efcce8690589@track-dev.pgyscm.com/2',
+    integrations: [
+      new Integrations.Vue({
+        Vue,
+        attachProps: true,
+      }),
+    ],
+  });
+}
+
 Vue.use(VueResource);
 Vue.config.productionTip = false;
 
