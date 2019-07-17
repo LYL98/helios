7b998cb616a06e780216e2193e26484a2b2a964d
胖墩-XXXXXXXXL <wx_9bbb4f3b386f4e0e8434a20e5378cc45@git.code.tencent.com>
2019-07-17 14:45:45 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -11,10 +11,10 @@ import * as Integrations from '@sentry/integrations';
 
 // 服务器地址
 let url = window.location.origin;
-let isPro = url.indexOf('apple.pgyscm.com') >= 0;
-if(!isPro){
+let isPro = url.indexOf('appledev.pgyscm.com') >= 0;
+if(isPro){
   Sentry.init({
-    dsn: 'http://3f58bbfce036428b83b8efcce8690589@track-dev.pgyscm.com/2',
+    dsn: 'https://3f58bbfce036428b83b8efcce8690589@track-dev.pgyscm.com/2',
     integrations: [
       new Integrations.Vue({
         Vue,
