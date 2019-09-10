f6cb41241396aaa2095f973e236a6398d0e4aae2
胖墩-XXXXXXXXL <wx_9bbb4f3b386f4e0e8434a20e5378cc45@git.code.tencent.com>
2019-09-10 17:00:24 +0800

    解决冲突

--- src/container/detail/index.js
+++ src/container/detail/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export { default as DetailItemGlobalEditRecord } from './DetailItemGlobalEditRecord';
 export { default as DetailItemListEditRecord } from './DetailItemListEditRecord';
 export { default as DetailItemGlobal } from './DetailItemGlobal';
@@ -13,12 +12,10 @@ export { default as DetailOperateReceivingAudit } from './DetailOperateReceiving
 
 export { default as DetailMerchantList } from './DetailMerchantList';
 export { default as DetailMerchantRefundLog } from './DetailMerchantRefundLog';
-=======
-export { default as DetailHeadBalanceLog } from './DetailHeadBalanceLog';
+
 export { default as DetailGroupItem } from './DetailGroupItem';
 export { default as DetailGroupItemEditLog } from './DetailGroupItemEditLog';
 export { default as DetailGroupActivity } from './DetailGroupActivity';
 export { default as DetailGroupActivityEditLog } from './DetailGroupActivityEditLog';
 export { default as DetailGroupActivityItem } from './DetailGroupActivityItem';
 export { default as DetailGroupStoreOrder } from './DetailGroupStoreOrder';
->>>>>>> develop
