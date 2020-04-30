1c51ef62085ada38494e0e336280d9a13789b82a
王璞 <501501369@qq.com>
2020-04-30 14:45:00 +0800

    优化分配装车模块的打印速度。

--- src/util/lodop.js
+++ src/util/lodop.js
@@ -84,140 +84,161 @@ export default {
     let LODOP = GET_LODOP();
     if (!LODOP) return;
 
+    try {
+      let stores = [];
+      // SKU 层 -> 遍历 批次列表
+      list.forEach(batch => {
 
-    let stores = [];
+        // 批次层 -> 遍历 线路列表
+        batch.items.forEach(line => {
 
-    // 批次层 -> 遍历 线路列表
-    list.forEach(batch => {
+          // 线路层 -> 遍历 门店列表
+          line.stores.forEach((store, index) => {
 
-      // 线路层 -> 遍历 门店列表
-      batch.items.forEach(line => {
+            // 为每个门店创建 打印任务
+            let taskList = [];
 
-        // 门店层 -> 遍历 门店列表 如果该门店是当前线路下的第一个门店，则额外添加线路信息，所有门店都添加商品信息到taskList
-        line.stores.forEach((store, index) => {
+            // 如果该门店是当前线路下的第一个门店，则额外添加线路信息
+            if (index === 0) {
+              taskList.push({
+                type: 'line',
+                line_index: prefixInteger(line.line_index, 2),
+                line_title: line.line_title,
+                line_num: line.num,
+                item_code: batch.item_code,
+              });
+            }
 
-          let taskList = [];
-          if (index === 0) {
-            taskList.push({
-              type: 'line',
+            let nums = Array.from({ length: store.num });
+            let task = {
+              type: 'item',
               line_index: prefixInteger(line.line_index, 2),
               line_title: line.line_title,
-              line_num: line.num,
+              city_index: prefixInteger(store.city_index, 2),
+              store_index: prefixInteger(store.store_index, 2),
+              store_title: (store.store.title || '').slice(0, 10),
               item_code: batch.item_code,
-            });
-          }
-          // 该门店的商品数量
-          let nums = Array.from({ length: store.num });
-          let task = {
-            type: 'item',
-            line_index: prefixInteger(line.line_index, 2),
-            line_title: line.line_title,
-            city_index: prefixInteger(store.city_index, 2),
-            store_index: prefixInteger(store.store_index, 2),
-            store_title: (store.store.title || '').slice(0, 10),
-            item_code: batch.item_code,
-            item_title: batch.item_title,
-            nums: store.num,
-            delivery_date: (delivery_date || '').slice(5, 10),
-            qrcontent: `{"type":"sort","out_stock_id":${batch.id},"item_id":${batch.item_id}}`
-          };
+              item_title: batch.item_title,
+              nums: store.num,
+              delivery_date: (delivery_date || '').slice(5, 10),
+              qrcontent: `{"type":"sort","out_stock_id":${batch.id},"item_id":${batch.item_id}}`
+            };
 
-          nums.forEach((d, index) => {
-            task.index = index + 1;
-            taskList.push({...task});
+            // 该门店的商品按件数 都添加到taskList （每件商品 对应 一个打印标签）
+            nums.forEach((d, index) => {
+              task.index = index + 1;
+              taskList.push({...task});
+
+              // 对件数 按25件单位 进行切割。
+              // 目的：门店任务列表的 标签数量不超过 25个。超过后，为该门店追加 新的taskList
+              if (index !== 0 && (index !== nums.length - 1) && ((index + 1) % 25 === 0)) {
+                stores.push([...taskList]);
+                taskList = [];
+              }
+            });
+
+            // 将所有的任务 添加到 stores（存放门店打印任务的 二维数组）
+            taskList.length > 0 && stores.push(taskList);
+
           });
 
-          stores.push(taskList);
 
         });
+      });
 
+      stores.forEach((taskList, index) => {
+        LODOP.PRINT_INIT('打印装车批次-配送门店');
+        taskList.forEach(task => {
+          if (task.type === 'line') {
+            LODOP.NewPage();
+            LODOP.SET_PRINT_PAGESIZE(1, 600, 400);
+            LODOP.SET_PRINT_STYLE('FontName', '微软雅黑');
+            LODOP.SET_PRINT_STYLE('FontSize', 18);
+            LODOP.SET_PRINT_STYLE('Bold', 1);
+            LODOP.SET_PRINT_STYLE('Alignment', 2);
+            LODOP.ADD_PRINT_TEXT(5, 0, 226, 60, `${task.line_index}/${task.line_title}`);
+            LODOP.SET_PRINT_STYLE('FontSize', 20);
+            LODOP.ADD_PRINT_TEXT(75, 0, 226, 30, `${task.line_num}件`);
+            LODOP.ADD_PRINT_TEXT(105, 0, 226, 30, task.item_code);
+          } else {
+            LODOP.NewPage();
+            LODOP.SET_PRINT_PAGESIZE(1, 600, 400);
+            LODOP.SET_PRINT_STYLE('FontName', '微软雅黑');
+            LODOP.SET_PRINT_STYLE('FontSize', 20);
+            LODOP.SET_PRINT_STYLE('Bold', 1);
+            LODOP.SET_PRINT_STYLE('Alignment', 2);
+            LODOP.ADD_PRINT_TEXT(5, 0, 226, 40, `${task.line_index}-${task.city_index}-${task.store_index}`);
+            LODOP.SET_PRINT_STYLE('FontSize', 9);
+            LODOP.SET_PRINT_STYLE('Alignment', 1);
+            LODOP.ADD_PRINT_TEXT(45, 5, 130, 20, task.store_title);
+            LODOP.SET_PRINT_STYLE('FontSize', 8);
+            LODOP.ADD_PRINT_TEXT(70, 5, 125, 50, `${task.item_code}/${task.item_title}`);
+            LODOP.ADD_PRINT_TEXT(125, 5, 125, 20, `${task.index} / ${task.nums}`);
+            LODOP.SET_PRINT_STYLE('Alignment', 3);
+            LODOP.ADD_PRINT_TEXT(125, 5, 125, 20, `蒲公英${task.delivery_date}`);
+            LODOP.ADD_PRINT_BARCODE(45, 135, 95, 95, 'QRCode', task.qrcontent);
+            LODOP.SET_PRINT_STYLEA(0, 'QRCodeVersion');
+          }
+
+        });
+        LODOP.SET_PRINT_MODE("CUSTOM_TASK_NAME","打印装车批次-配送门店"+index);
+        LODOP.PRINT();
 
       });
-    });
 
-    stores.forEach((taskList, index) => {
-      LODOP.PRINT_INIT('打印装车批次-配送门店');
-      taskList.forEach(task => {
-        if (task.type === 'line') {
-          LODOP.NewPage();
-          LODOP.SET_PRINT_PAGESIZE(1, 600, 400);
-          LODOP.SET_PRINT_STYLE('FontName', '微软雅黑');
-          LODOP.SET_PRINT_STYLE('FontSize', 18);
-          LODOP.SET_PRINT_STYLE('Bold', 1);
-          LODOP.SET_PRINT_STYLE('Alignment', 2);
-          LODOP.ADD_PRINT_TEXT(5, 0, 226, 60, `${task.line_index}/${task.line_title}`);
-          LODOP.SET_PRINT_STYLE('FontSize', 20);
-          LODOP.ADD_PRINT_TEXT(75, 0, 226, 30, `${task.line_num}件`);
-          LODOP.ADD_PRINT_TEXT(105, 0, 226, 30, task.item_code);
-        } else {
-          LODOP.NewPage();
-          LODOP.SET_PRINT_PAGESIZE(1, 600, 400);
-          LODOP.SET_PRINT_STYLE('FontName', '微软雅黑');
-          LODOP.SET_PRINT_STYLE('FontSize', 20);
-          LODOP.SET_PRINT_STYLE('Bold', 1);
-          LODOP.SET_PRINT_STYLE('Alignment', 2);
-          LODOP.ADD_PRINT_TEXT(5, 0, 226, 40, `${task.line_index}-${task.city_index}-${task.store_index}`);
-          LODOP.SET_PRINT_STYLE('FontSize', 9);
-          LODOP.SET_PRINT_STYLE('Alignment', 1);
-          LODOP.ADD_PRINT_TEXT(45, 5, 130, 20, task.store_title);
-          LODOP.SET_PRINT_STYLE('FontSize', 8);
-          LODOP.ADD_PRINT_TEXT(70, 5, 125, 50, `${task.item_code}/${task.item_title}`);
-          LODOP.ADD_PRINT_TEXT(125, 5, 125, 20, `${task.index} / ${task.nums}`);
-          LODOP.SET_PRINT_STYLE('Alignment', 3);
-          LODOP.ADD_PRINT_TEXT(125, 5, 125, 20, `蒲公英${task.delivery_date}`);
-          LODOP.ADD_PRINT_BARCODE(45, 135, 95, 95, 'QRCode', task.qrcontent);
-          LODOP.SET_PRINT_STYLEA(0, 'QRCodeVersion');
-        }
-
-      });
-      LODOP.SET_PRINT_MODE("CUSTOM_TASK_NAME","打印装车批次-配送门店"+index);
-      LODOP.PRINT();
-
-    });
+    } catch (e) {
+      console.error('CLODOP PRINT ERROR: ', e);
+    }
 
   },
 
   tempTruckBatchPreview: function(list, delivery_date) {
     let LODOP = GET_LODOP();
     if (!LODOP) return;
-    LODOP.PRINT_INIT('打印装车批次');
 
-    list.forEach(d => {
-      let qrcontent = `{"type":"sort","out_stock_id":${d.id},"item_id":${d.item_id}}`;
-      d.items.forEach(item => {
-        LODOP.NewPage();
-        LODOP.SET_PRINT_PAGESIZE(1, 600, 400);
-        LODOP.SET_PRINT_STYLE('FontName', '微软雅黑');
-        LODOP.SET_PRINT_STYLE('FontSize', 18);
-        LODOP.SET_PRINT_STYLE('Bold', 1);
-        LODOP.SET_PRINT_STYLE('Alignment', 2);
-        LODOP.ADD_PRINT_TEXT(5, 0, 226, 60, `${prefixInteger(item.line_index, 2)}/${item.line_title}`);
-        LODOP.SET_PRINT_STYLE('FontSize', 20);
-        LODOP.ADD_PRINT_TEXT(75, 0, 226, 30, `${item.num}件`);
-        LODOP.ADD_PRINT_TEXT(105, 0, 226, 30, d.item_code);
-        item.stores.forEach(store => {
-          let nums = Array.from({length: store.num});
-          nums.forEach((n, i) => {
-            LODOP.NewPage();
-            LODOP.SET_PRINT_STYLE('FontName', '微软雅黑');
-            LODOP.SET_PRINT_STYLE('FontSize', 20);
-            LODOP.SET_PRINT_STYLE('Bold', 1);
-            LODOP.SET_PRINT_STYLE('Alignment', 2);
-            LODOP.ADD_PRINT_TEXT(5, 0, 226, 40, `${prefixInteger(item.line_index, 2)}-${prefixInteger(store.city_index, 2)}-${prefixInteger(store.store_index, 2)}`);
-            LODOP.SET_PRINT_STYLE('FontSize', 9);
-            LODOP.SET_PRINT_STYLE('Alignment', 1);
-            LODOP.ADD_PRINT_TEXT(45, 5, 130, 20, (store.store.title || '').slice(0, 10));
-            LODOP.SET_PRINT_STYLE('FontSize', 8);
-            LODOP.ADD_PRINT_TEXT(70, 5, 125, 50, `${d.item_code}/${d.item_title}`);
-            LODOP.ADD_PRINT_TEXT(125, 5, 125, 20, `${i+1} / ${store.num}`);
-            LODOP.SET_PRINT_STYLE('Alignment', 3);
-            LODOP.ADD_PRINT_TEXT(125, 5, 125, 20, `蒲公英${(delivery_date || '').slice(5, 10)}`);
-            LODOP.ADD_PRINT_BARCODE(45, 135, 95, 95, 'QRCode', qrcontent);
-            LODOP.SET_PRINT_STYLEA(0, 'QRCodeVersion');
-          });
+    try {
+      LODOP.PRINT_INIT('打印装车批次');
+
+      list.forEach(d => {
+        let qrcontent = `{"type":"sort","out_stock_id":${d.id},"item_id":${d.item_id}}`;
+        d.items.forEach(item => {
+          LODOP.NewPage();
+          LODOP.SET_PRINT_PAGESIZE(1, 600, 400);
+          LODOP.SET_PRINT_STYLE('FontName', '微软雅黑');
+          LODOP.SET_PRINT_STYLE('FontSize', 18);
+          LODOP.SET_PRINT_STYLE('Bold', 1);
+          LODOP.SET_PRINT_STYLE('Alignment', 2);
+          LODOP.ADD_PRINT_TEXT(5, 0, 226, 60, `${prefixInteger(item.line_index, 2)}/${item.line_title}`);
+          LODOP.SET_PRINT_STYLE('FontSize', 20);
+          LODOP.ADD_PRINT_TEXT(75, 0, 226, 30, `${item.num}件`);
+          LODOP.ADD_PRINT_TEXT(105, 0, 226, 30, d.item_code);
+          item.stores.forEach(store => {
+            let nums = Array.from({length: store.num});
+            nums.forEach((n, i) => {
+              LODOP.NewPage();
+              LODOP.SET_PRINT_STYLE('FontName', '微软雅黑');
+              LODOP.SET_PRINT_STYLE('FontSize', 20);
+              LODOP.SET_PRINT_STYLE('Bold', 1);
+              LODOP.SET_PRINT_STYLE('Alignment', 2);
+              LODOP.ADD_PRINT_TEXT(5, 0, 226, 40, `${prefixInteger(item.line_index, 2)}-${prefixInteger(store.city_index, 2)}-${prefixInteger(store.store_index, 2)}`);
+              LODOP.SET_PRINT_STYLE('FontSize', 9);
+              LODOP.SET_PRINT_STYLE('Alignment', 1);
+              LODOP.ADD_PRINT_TEXT(45, 5, 130, 20, (store.store.title || '').slice(0, 10));
+              LODOP.SET_PRINT_STYLE('FontSize', 8);
+              LODOP.ADD_PRINT_TEXT(70, 5, 125, 50, `${d.item_code}/${d.item_title}`);
+              LODOP.ADD_PRINT_TEXT(125, 5, 125, 20, `${i+1} / ${store.num}`);
+              LODOP.SET_PRINT_STYLE('Alignment', 3);
+              LODOP.ADD_PRINT_TEXT(125, 5, 125, 20, `蒲公英${(delivery_date || '').slice(5, 10)}`);
+              LODOP.ADD_PRINT_BARCODE(45, 135, 95, 95, 'QRCode', qrcontent);
+              LODOP.SET_PRINT_STYLEA(0, 'QRCodeVersion');
+            });
+          })
         })
-      })
-    });
-    return LODOP;
+      });
+      return LODOP;
+
+    } catch (e) {
+      console.error('CLODOP PREVIEW ERROR: ', e);
+    }
   }
 }
